df159ad31879e26ffa62aa58f3075321bb5f175d
Anjaly Sam <anjalysam07@gmail.com>
2020-07-22 11:49:47 +0530

    final commit

---
+++ plot1.R
@@ -0,0 +1,10 @@
+#set up working enviorment
+setwd("F://data science _online course//intershala")
+#readind the data
+data=read.table("household_power_consumption.txt",header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+head(data)
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
